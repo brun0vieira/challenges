6a7c01bd9ba0d5864a531866c74940a9cb83c85f
brun0vieira <bar.vieira@campus.fct.unl.pt>
2022-12-07 22:17:32 +0000

    solve 1x

---
+++ advent_of_code/2022/01.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+nums = [n.rstrip('\n') for n in open("01_input.txt")]
+
+aux, elfs = 0, []
+for n in nums:
+    if n != '':
+        aux += int(n)
+    else:
+        elfs.append(aux)
+        aux = 0
+
+ind = np.argmax(elfs)
+max = elfs[ind]
+print(max)
+
+for i in range(2):
+    elfs.pop(ind)
+    ind = np.argmax(elfs)
+    max += elfs[ind]
+
+print(max)
