658fbddaa05e8f82fe9f57b0a4354130c66ae96a
brun0vieira <bar.vieira@campus.fct.unl.pt>
2022-12-08 23:40:55 +0000

    prob 2 poorly solved :(

---
+++ advent_of_code/2022/02.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+input = [n.rstrip('\n') for n in open("02_input.txt")]
+
+rock, paper, scissors = ['A', 'X'], ['B', 'Y'], ['C', 'Z']
+wins, points = ['C X', 'A Y', 'B Z'], {'X':1, 'Y':2, 'Z':3}
+
+score1 = 0
+score2 = 0
+for x in input:
+    x1, x2 = x.split(' ')
+    play1 = [x1, x2]
+
+    score1 += points[x2]
+    if x in wins:
+        score1 += 6
+    elif play1 in [rock, paper, scissors]:
+        score1 += 3
+
+    if x2 == 'X':
+        if x1 == 'A':
+            score2 += points['Z']
+        elif x1 == 'B':
+            score2 += points['X']
+        else:
+            score2 += points['Y']
+    elif x2 == 'Y':
+        score2 += 3
+        if x1 == 'A':
+            score2 += points['X']
+        elif x1 == 'B':
+            score2 += points['Y']
+        else:
+            score2 += points['Z'] 
+    else:
+        score2 += 6
+        if x1 == 'A':
+            score2 += points['Y']
+        elif x1 == 'B':
+            score2 += points['Z']
+        else:
+            score2 += points['X']
+
+print(score1)
+print(score2)
---
+++ advent_of_code/2022/template.py
@@ -0,0 +1,3 @@
+import numpy as np
+
+input = [n.rstrip('\n') for n in open("02_input.txt")]
