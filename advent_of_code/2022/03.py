1e8c57c2bec7f33c2b2a3845c2a479fa030de4e5
brun0vieira <bar.vieira@campus.fct.unl.pt>
2022-12-09 18:43:20 +0000

    clean up

--- advent_of_code/2022/03.py
+++ advent_of_code/2022/03.py
@@ -1,10 +1,10 @@
 input = [n.rstrip('\n') for n in open("03_input.txt")]
 
 def get_common(a, b):
-    return list(set(a)&set(b))
+    return list(set(a)&set(b))[0]
 
 def get_common_3(a, b, c):
-    return list(set(a)&set(b)&set(c))
+    return list(set(a)&set(b)&set(c))[0]
 
 def get_priority(character):
     if character.islower():
@@ -15,14 +15,14 @@ def get_priority(character):
 result, result2, counter, buffer = 0, 0, 0, []
 for i in input:
     a, b = i[:len(i)//2], i[len(i)//2:]
-    common = get_common(a, b)[0]
+    common = get_common(a, b)
     result += get_priority(common)
 
     buffer.append(i)
     counter += 1
     if counter == 3:
         common = get_common_3(buffer[0], buffer[1], buffer[2])
-        result2 += get_priority(common[0])
+        result2 += get_priority(common)
         buffer, counter = [], 0
 
 print(result)
