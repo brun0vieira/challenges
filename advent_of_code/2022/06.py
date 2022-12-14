4228c48817cec368d689dcf43a0d5cf05610d4c5
brun0vieira <bar.vieira@campus.fct.unl.pt>
2022-12-14 19:33:06 +0000

    that was easy

--- advent_of_code/2022/06.py
+++ advent_of_code/2022/06.py
@@ -1,15 +1,17 @@
 f = open("06_input.txt", "r")
 input = f.readlines()[0]
-stack, result1 = [], 0
+stack = []
 
-for i in range(len(input)):
-    stack.append(input[i])
-    if len(stack) != 4:
-        continue
-    elif len(set(stack)) == len(stack):
-        result1 = i+1
-        break
-    else:
-        stack.pop(0)
+def get_marker(part1=True):
+    nr = 4 if part1 else 14   
+    for i in range(len(input)):
+        stack.append(input[i])
+        if len(stack) != nr:
+            continue
+        elif len(set(stack)) == len(stack):
+            return i+1
+        else:
+            stack.pop(0)
 
-print(result1)
+print(get_marker())
+print(get_marker(False))
